49e72316bde0d2e8f9dc4202d5a864269e1cdf4c
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2020-08-01 02:18:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ function  calcular(){
     
     var hirkin = document.getElementById('dhk')
     var yakin  = document.getElementById('dyk')
-    
+    var yukyu = document.getElementById('dyku')
     var hirkindo = document.getElementById('dhkdo')
     var yakindo  = document.getElementById('dykdo')
 
-    var sahirkin = ((Number(salHor.value)*9 + ((Number(salHor.value)*1.25)*2)) * Number(hirkin.value))
-    var sayakin = ((Number(salHor.value)*1.5 + ((Number(salHor.value)*1.25)*9.5)) * Number(yakin.value))
-    var sahirkindo = ((((Number(salHor.value)*1.35)*9 )+ ((Number(salHor.value)*1.6)*2)) * Number(hirkindo.value)) 
-    var sayakindo = ((((Number(salHor.value)*1.35)*1.5) + ((Number(salHor.value)*1.6)*9.5)) * Number(yakindo.value))
-    var salBru = sahirkin + sayakin + sahirkindo + sayakindo
+    var sahirkin = ((Number(salHor.value)*8.8 + ((Number(salHor.value)*1.25)*2)) * Number(hirkin.value))
+    var sayakin = ((Number(salHor.value)*1.5 + ((Number(salHor.value)*1.25)*9.3)) * Number(yakin.value))
+    var sahirkindo = ((((Number(salHor.value)*1.35)*8.8)+ ((Number(salHor.value)*1.6)*2)) * Number(hirkindo.value)) 
+    var sayakindo = ((((Number(salHor.value)*1.35)*1.5) + ((Number(salHor.value)*1.6)*9.3)) * Number(yakindo.value))
+    var sayukil = ((Number(salHor.value)*8.8) * Number(yukyu.value))
+    var salBru = sahirkin + sayakin + sahirkindo + sayakindo + sayukil
     var diastr = Number(hirkin.value) + Number(yakin.value) + Number(hirkindo.value) + Number(yakindo.value)
+    
    
     sahirkin = sahirkin.toLocaleString('ja-JP',{style: 'currency', currency: 'JPY'});
     sayakin= sayakin.toLocaleString('ja-JP',{style: 'currency', currency: 'JPY'});
     sahirkindo = sahirkindo.toLocaleString('ja-JP',{style: 'currency', currency: 'JPY'});
     sayakindo = sayakindo.toLocaleString('ja-JP',{style: 'currency', currency: 'JPY'});
+    sayukil = sayukil.toLocaleString('ja-JP',{style: 'currency', currency: 'JPY'});
     salBru = salBru.toLocaleString('ja-JP',{style: 'currency', currency: 'JPY'});
+    
    
     dtr.innerHTML =`Dias traballhados:`
     dtrv.innerHTML=`${diastr}`
@@ -35,6 +39,10 @@ function  calcular(){
     syd.innerHTML =`Yakin Domingo:`
     sydv.innerHTML = `${sayakindo}`
     
+    syyk.innerHTML =`Salario Yukyu:`
+    syykv.innerHTML = `${sayukil}`
+    
+
     sbr.innerHTML =`Salario Bruto:`
     sbrv.innerHTML =`${salBru}`
 
