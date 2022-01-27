4c8b8648c924c423b976441a4052f7ec2216351c
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2022-01-27 17:17:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 function  calcular(){
+
+
+
     var salHor = document.getElementById('valhora')
     
     var hirkin = document.getElementById('dhk')
     var yakin  = document.getElementById('dyk')
     var yukyu = document.getElementById('dyku')
+    var folgfor = document.getElementById('dfor')
     var hirkindo = document.getElementById('dhkdo')
     var yakindo  = document.getElementById('dykdo')
+    var percen = document.getElementsByName('radfor')
+    var porcent = 0 
+    if(percen[0].checked){
+        porcent = 0.80}
+        else{
+            porcent = 0.60
+        }
 
-    var sahirkin = ((Number(salHor.value)*8.3333333)+ ((Number(salHor.value)*1.25)*2)) * Number(hirkin.value)
-    var sayakin = ((Number(salHor.value)*3.1666666) + ((Number(salHor.value)*1.25)*7.3333333)) * Number(yakin.value)
-    var sahirkindo = ((((Number(salHor.value)*1.35)*8.33333)+ ((Number(salHor.value)*1.6)*2)) * Number(hirkindo.value)) 
-    var sayakindo = ((((Number(salHor.value)*1.35)*3.1666666) + ((Number(salHor.value)*1.6)*7.3333333)) * Number(yakindo.value))
+
+
+    var sahirkin = ((Number(salHor.value)*9)+ ((Number(salHor.value)*1.25)*2)) * Number(hirkin.value)
+    var sayakin = ((Number(salHor.value)*9) + ((Number(salHor.value)*0.25)*5.833333334) + (Number(salHor.value)* 1.25 * 2)) * Number(yakin.value)
+    var sahirkindo = (((Number(salHor.value)*1.35)*11) * Number(hirkindo.value)) 
+    var sayakindo = ((((Number(salHor.value)*1.35)*11) + ((Number(salHor.value)*0.25)*5.833333334)) * Number(yakindo.value))
     var sayukil = ((Number(salHor.value)*9) * Number(yukyu.value))
-    var salBru = sahirkin + sayakin + sahirkindo + sayakindo + sayukil
+    var safolfor = (((Number(salHor.value)*9)*porcent) * Number(folgfor.value))
+    var salBru = sahirkin + sayakin + sahirkindo + sayakindo + sayukil + safolfor
     var diastr = Number(hirkin.value) + Number(yakin.value) + Number(hirkindo.value) + Number(yakindo.value)
     
    
